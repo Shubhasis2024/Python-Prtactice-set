a36408e3ff8a8fadcea1534d6d4768bcd3ab87e6
Shubhasis Ghosh <149863773+Shubhasis2024@users.noreply.github.com>
2025-06-28 17:04:23 +0530

    All uploaded

---
+++ practice.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the first number:" ))
+num2 = int (input("2Enter the second number:" )) 
+
+print("The sum of", num1, "and", num2, "is", num1 + num2)  
+
+print("The reminder is which is diveded by", num2 , " to the", num1," is:", num1 % num2)
+
