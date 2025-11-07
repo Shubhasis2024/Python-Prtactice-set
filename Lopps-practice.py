89269327b78a9bc958f0b7d0df6a6e60fda5c398
Shubhasis Ghosh <149863773+Shubhasis2024@users.noreply.github.com>
2025-11-07 13:50:25 +0530

    Add files via upload

---
+++ Lopps-practice.py
@@ -0,0 +1,33 @@
+# for i in range (1,11):
+#     print(f"{Num}x{i}={Num*i}")
+
+
+# Num=int(input("Enter any number :"))
+# for i in range (2,Num):
+#     if(Num%i)==0:
+#         print("The number is the not prime ")
+#         break
+#     else:
+#         print("The number is prime number")
+
+
+
+num=int(input("Enter number any :"))
+# i=1
+# sum=0
+# while(i<=num):
+#     sum +=i
+#     i +=1
+# print("Sum:",sum)
+
+# product=1
+# for i in range(1,num+1):
+#     product= product*i
+# print(f"The factorial of {num} is {product} ")
+# for i in range (1,num+1):
+#     print(" "*(num-i),end="")
+#     print("*"*(2*i-1),end="")
+#     print("")
+
+for i in range (1,11):
+    print(f"{num}x{11-i}={num*(11-i)}")
