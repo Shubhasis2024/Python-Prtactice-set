91c3e21ef8b972828862ee51dbe3d4c39efd3344
Shubhasis Ghosh <149863773+Shubhasis2024@users.noreply.github.com>
2025-11-25 22:25:57 +0530

    Add files via upload

---
+++ Recurssion.py
@@ -0,0 +1,35 @@
+# def factorial(n):
+#     if (n==1 or n==0):
+#         return 1
+#     return n*factorial(n-1)
+
+
+# n=int(input("Enter any number :"))
+# print(f"The factorial of {n} is :{factorial(n)}")
+
+# def naturalsum(n):
+#     if (n==1):
+#         return 1
+#     return naturalsum(n-1)+ n
+    
+    
+# n=int(input("Enter here:"))
+# print(f"Fist sum of this {n} no is : {naturalsum(n)}")
+
+ 
+# def star(num):
+#     if (num==0):
+#         return
+#     print("*"*num)
+#     star(num-1)
+
+ 
+#  def star2(num):
+#     if (num==0):
+#         return
+#     star2(num-1)
+#     print("*"*num)
+
+# star(3)
+# star2(3)
+
